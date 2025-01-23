c8a33794a60e65cf4a4ef357d2abe935ebfd6255
muskan7061 <ansarimuskan7061@gmail.com>
2025-01-23 12:31:09 +0530

    Initial commit

---
+++ src/demo.js
@@ -0,0 +1,66 @@
+<ul class="timeline timeline-vertical lg:timeline-horizontal">
+  <li>
+    <div class="timeline-start text-base-content font-medium">1984</div>
+    <div class="timeline-middle">
+      <span class="bg-primary/20 flex size-4.5 items-center justify-center rounded-full">
+        <span class="badge badge-primary size-3 rounded-full p-0"></span>
+      </span>
+    </div>
+    <div class="timeline-end timeline-box">Macintosh PC</div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div class="timeline-start text-base-content font-medium">1998</div>
+    <div class="timeline-middle">
+      <span class="bg-primary/20 flex size-4.5 items-center justify-center rounded-full">
+        <span class="badge badge-primary size-3 rounded-full p-0"></span>
+      </span>
+    </div>
+    <div class="timeline-end timeline-box">iMac</div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div class="timeline-start text-base-content font-medium">2001</div>
+    <div class="timeline-middle">
+      <span class="bg-primary/20 flex size-4.5 items-center justify-center rounded-full">
+        <span class="badge badge-primary size-3 rounded-full p-0"></span>
+      </span>
+    </div>
+    <div class="timeline-end timeline-box">iPod</div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div class="timeline-start text-base-content font-medium">2007</div>
+    <div class="timeline-middle">
+      <span class="bg-primary/20 flex size-4.5 items-center justify-center rounded-full">
+        <span class="badge badge-primary size-3 rounded-full p-0"></span>
+      </span>
+    </div>
+    <div class="timeline-end timeline-box">iPhone</div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div class="timeline-start text-base-content font-medium">2015</div>
+    <div class="timeline-middle">
+      <span class="bg-primary/20 flex size-4.5 items-center justify-center rounded-full">
+        <span class="badge badge-primary size-3 rounded-full p-0"></span>
+      </span>
+    </div>
+    <div class="timeline-end timeline-box">Apple Watch</div>
+    <hr />
+  </li>
+  <li>
+    <hr />
+    <div class="timeline-start text-base-content font-medium">2024</div>
+    <div class="timeline-middle">
+      <span class="bg-primary/20 flex size-4.5 items-center justify-center rounded-full">
+        <span class="badge badge-primary size-3 rounded-full p-0"></span>
+      </span>
+    </div>
+    <div class="timeline-end timeline-box">Vision Pro</div>
+  </li>
+</ul>
---
+++ src/index.js
@@ -0,0 +1,44 @@
+// require('dotenv').config({path: './env'})
+import dotenv from "dotenv"
+import connectDB from "./db/index.js"
+
+dotenv.config({path: './env'})
+
+connectDB()
+.then(() => {
+    app.listen(process.env.PORT|| 8000,  () => {
+        console.log(`app is listing on ${process.env.PORT}`);
+        
+    })
+})
+.catch((err) =>{
+    console.log("MOngoDB connection FAILED", err);
+
+})
+
+
+
+/*
+import express from "express"
+const app = express()
+
+
+( async () => {
+    try{
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error=>", (err) =>{
+            console.log("Error=>", err);
+            throw err
+            
+        })
+        app.listen(process.env.PORT, () => {
+            console.log(`app is listing on ${process.env.PORT}`);
+            
+        })
+
+    } catch (error) {
+        console.log("ERROR: ", error);
+        throw err
+        
+    }
+}) ()*/
